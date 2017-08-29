66dfb8e702b97b59f24407ff395d339aa0abea32
Jesse Keating <jkeating@j2solutions.net>
2017-08-29 15:43:50 -0700

    Update sendit to sign the body
    Zuul requires that the body be signed, so sign it.

--- sendit.py
+++ sendit.py
@@ -2,6 +2,8 @@
 #
 
 import argparse
+import hashlib
+import hmac
 import json
 import requests
 import os
@@ -15,14 +17,26 @@ parser.add_argument('event', help='The type of GitHub event',
 parser.add_argument('filename', help='The json file to send')
 parser.add_argument('--url', help='The URL to send.',
                     default='http://localhost:8001/connection/github/payload')
+parser.add_argument('--token', help='Webhook secret for signing',
+                    default='00000000000000000000000000000000000000000')
 
 options = parser.parse_args()
 
 with open(os.path.expanduser(options.filename)) as json_file:
     data = json.load(json_file)
 
-s = requests.session()
-s.headers.update({'Content-Type': 'application/json',
-                  'X-Github-Event': options.event})
 
-s.post(options.url, json=data)
+request = requests.Request('POST', options.url, json=data).prepare()
+
+signature = 'sha1=' + hmac.new(options.token.encode('utf-8'), request.body,
+                               hashlib.sha1).hexdigest()
+
+request.headers.update({'Content-Type': 'application/json',
+                  'X-Github-Event': options.event,
+                  'X-Hub-Signature': signature,
+                  'X-GitHub-Delivery': 'derp'})
+
+with requests.Session() as s:
+    resp = s.send(request)
+
+print(resp)
